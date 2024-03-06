0ee84f063ecdcfaa216ad91bbf55ef24e3e8b45a
Nadzz <116545553+Mugweru-43@users.noreply.github.com>
2024-03-06 16:16:30 +0300

    Update and rename README.md to user_input.py

---
+++ user_input.py
@@ -0,0 +1,13 @@
+#user_input.py
+
+#Ask user for their name
+name = input("What is your name?")
+
+#Ask user for their age
+age = input("How old are you?")
+
+#Ask user for their location
+locatio = input("Where are you located?")
+
+#Print personalized message
+print(f" (Nadine), you are (22) years old and live in(Embakasi).")
